cd238c803ca4430809a866fb799c09177beb9e44
stacielea <156947901+stacielea@users.noreply.github.com>
2024-01-23 19:42:52 +0000

    save progress

---
+++ M1HW1_Tyndall.cpp
@@ -0,0 +1,24 @@
+/*
+CSC 134
+M1LAB - Apple Sales
+S Tyndall
+1/22/24
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*
+    Variables:
+    something to hold the name of the movie
+    something to hold the year that it came out
+    something to hold the box office gross or some other monetary figure about the movie (using a decimal point)
+    */
+    string movie_name = "Rocky IV";
+    string movie_year = "1985";
+    string box_office_gross = "300.4 million dollars"
+
+    // display information
+
+    return 0;
+}
