c9394ff2e7cc6b6d10f11c48442d7770d1f700e4
stacielea <156947901+stacielea@users.noreply.github.com>
2024-02-15 16:09:59 +0000

    not done

---
+++ M2HW_Tyndall.cpp
@@ -0,0 +1,47 @@
+/*
+CSC 134
+M2HW - Silver
+S Tyndall
+2/15/24
+*/
+#include <iostream>
+using namespace std;
+/*
+ Question one
+ Question two
+ Question four
+*/
+
+int main() {
+    Question_four();
+    
+  return 0; 
+
+} 
+
+// Question one
+void Question_one(){
+
+
+}
+
+// Question two
+void Question_two(){
+
+
+}
+
+// Question four
+void Question_four(){
+    string school, team, lets_go,cheer_one, cheer_two;
+    lets_go = "Let's go";
+    school = "FTCC";
+    team = "Trojans";
+    cheer_one = lets_go + " " + school + "!";
+    cheer_two = lets_go + " " + team + "!";
+    cout << cheer_one << endl;
+    cout << cheer_one << endl;
+    cout << cheer_one << endl;
+    cout << cheer_two << endl;
+
+}
